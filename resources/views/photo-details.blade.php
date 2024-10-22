055861716417f67cf13ac822e53354aa4c37868a
atiikmahmud <atikmahmud.cse@gmail.com>
2024-10-22 15:33:50 +0600

    minor updated

--- resources/views/photo-details.blade.php
+++ resources/views/photo-details.blade.php
@@ -20,11 +20,6 @@
             </div>
             <div class="col-xl-4 col-lg-5 col-md-6 col-sm-12">
                 <div class="tm-bg-gray tm-video-details">
-                    <p class="mb-4">
-                        Please support us by making <a href="https://paypal.me/templatemo" target="_parent"
-                            rel="sponsored">a PayPal donation</a>. Nam ex nibh, efficitur eget libero ut, placerat aliquet
-                        justo. Cras nec varius leo.
-                    </p>
                     <div class="text-center mb-5">
                         <a href="#" class="btn btn-primary tm-btn-big">Download</a>
                     </div>
