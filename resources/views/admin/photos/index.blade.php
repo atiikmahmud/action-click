a13877c3ed72960bcccf8ffdecbda3ce78faa0a4
atiikmahmud <atikmahmud.cse@gmail.com>
2024-11-06 18:33:44 +0600

    Photo modal added

--- resources/views/admin/photos/index.blade.php
+++ resources/views/admin/photos/index.blade.php
@@ -15,8 +15,8 @@
                             class="form-control border-0 shadow-none" placeholder="Search..." aria-label="Search..." />
                         <button type="submit" class="d-none">Submit</button>
                     </form>
-                    @if($search)
-                    <a href="{{ route('admin.photos.index') }}"><i class="ri-delete-back-2-line"></i></a>
+                    @if ($search)
+                        <a href="{{ route('admin.photos.index') }}"><i class="ri-delete-back-2-line"></i></a>
                     @endif
                 </div>
                 <!-- /Search -->
@@ -46,8 +46,8 @@
                                     <td>{{ $index + $photos->firstItem() }}</td>
 
                                     <td><img src="{{ $photo->image ? asset('admin-assets/img/photos/' . $photo->image) : asset('admin-assets/img/avatars/1.png') }}"
-                                            alt="{{ $photo->name }}" class="w-px-50 h-auto"
-                                            style="margin-right: 5px" />
+                                            alt="{{ $photo->name }}" class="w-px-50 h-auto" style="margin-right: 5px"
+                                            data-bs-toggle="modal" data-bs-target="#{{ $photo->id }}" />
                                         <span>{{ $photo->name }}</span>
                                     </td>
                                     <td>{{ $photo->tag }}</td>
@@ -62,12 +62,14 @@
                                     </td>
                                     <td>
                                         @if ($photo->featured == 1)
-                                            <span class="badge rounded-pill bg-label-success me-1"><i class="ri-star-fill"></i></span>
+                                            <span class="badge rounded-pill bg-label-success me-1"><i
+                                                    class="ri-star-fill"></i></span>
                                         @else
-                                            <span class="badge rounded-pill bg-label-danger me-1"><i class="ri-star-line"></i></span>
+                                            <span class="badge rounded-pill bg-label-danger me-1"><i
+                                                    class="ri-star-line"></i></span>
                                         @endif
                                     </td>
-                                    <td>{{ $photo->created_at }}</td>
+                                    <td>{{ date('d M Y', strtotime($photo->created_at)) }}</td>
                                     <td>
                                         <div class="dropdown">
                                             <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
@@ -88,6 +90,44 @@
                                             </div>
                                         </div>
                                     </td>
+
+                                    <!-- Extra Large Modal -->
+                                    <div class="modal fade" id="{{ $photo->id }}" tabindex="-1" aria-hidden="true">
+                                        <div class="modal-dialog modal-xl" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLabel4">
+                                                        {{ $photo->name }}
+                                                    </h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <div class="row g-0">
+                                                        <div class="col-md-8">
+                                                            <img class="card-img rounded"
+                                                                src="{{ asset('admin-assets/img/photos/' . $photo->image) }}"
+                                                                alt="{{ $photo->name }}" />
+                                                        </div>
+                                                        <div class="col-md-4">
+                                                            <div class="card-body">
+                                                                <p><strong>Title: </strong>{{ $photo->name }}</p>
+                                                                <p><strong>Tag: </strong>{{ $photo->tag }}</p>
+                                                                <p><strong>Date: </strong>{{ date('d M Y', strtotime($photo->created_at)) }}</p>
+                                                                <p><strong>Title: </strong>{{ $photo->name }}</p>
+                                                                
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-outline-success" data-bs-dismiss="modal">Approve</button>
+                                                    <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Reject</button>
+                                                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </tr>
                             @endforeach
                         @else
