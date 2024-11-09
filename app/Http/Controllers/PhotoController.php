068a968ff8b82fe58f97405e6b863eb1e656c0c8
atiikmahmud <atikmahmud.cse@gmail.com>
2024-11-10 00:10:33 +0600

    photo view count added

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -16,6 +16,9 @@ class PhotoController extends Controller
     public function photoDetails($id)
     {
         $photo = Photos::with('user')->where('id', $id)->first();
+        $view = 1;
+        $photo->view_count += $view; 
+        $photo->save();
         $title = $photo->name . ' - Photo Details';
         $related_photos = Photos::where('id', '!=', '$photo->id')->take(8)->get();
         return view('photos.photo-details', compact('title', 'photo', 'related_photos'));
